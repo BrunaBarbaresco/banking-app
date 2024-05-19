fdf48b2871a5a7199fa26b3edd3aea00d82d361e
BrunaBarbaresco <44330308+BrunaBarbaresco@users.noreply.github.com>
2024-05-18 23:14:39 -0300

    Links da sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const SideBar = ({ user }: SidebarProps) => {
+  const pathname = usePathname();
+
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -16,7 +22,33 @@ const SideBar = ({ user }: SidebarProps) => {
           />
           <h1 className="sidebar-logo">Horizon</h1>
         </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+              href={item.route}
+              key={item.label}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+        USER
       </nav>
+      FOOTER
     </section>
   );
 };
