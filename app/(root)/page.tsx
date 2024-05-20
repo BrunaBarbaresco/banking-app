78ad8cf07817db3e4ec4727243990c2ac50ead3e
BrunaBarbaresco <44330308+BrunaBarbaresco@users.noreply.github.com>
2024-05-20 13:06:06 -0300

    Implementação dos componentes `RightSidebar` e `BankCard`

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,39 @@
 import HeaderBox from "@/components/HeaderBox";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Bruna" };
+  const loggedIn = {
+    firstName: "Bruna",
+    lastName: "Barbaresco",
+    email: "emailteste@xxxxx.com",
+    gender: "f",
+  };
 
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Bem vindo"
-          user={loggedIn?.firstName || "Convidado"}
-          subtext="Acesse e gerencie sua conta e suas transações de forma eficiente!"
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title={loggedIn.gender === "m" ? "Bem vindo," : "Bem vinda,"}
+            user={`${loggedIn?.firstName}!` || "Convidado"}
+            subtext="Acesse e gerencie sua conta e suas transações de forma eficiente!"
+          />
 
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={27750.0}
-        />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={3}
+            totalCurrentBalance={127750.0}
+          />
+        </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 186754.5 }, { currentBalance: 161754.5 }]}
+      />
     </section>
   );
 };
