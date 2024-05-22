15f892add3dd778d8279d33c83f329ba923618c0
BrunaBarbaresco <44330308+BrunaBarbaresco@users.noreply.github.com>
2024-05-22 15:16:42 -0300

    Atualização do componente `AuthForm.tsx` com todos os campos e utilização do `CustomInput`

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 
@@ -8,33 +8,29 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-
-const formSchema = z.object({
-  email: z.string().email(),
-});
+import { Form } from "@/components/ui/form";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+import Link from "next/link";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
     console.log(values);
+    setIsLoading(false);
   }
 
   return (
@@ -72,28 +68,101 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
+              {type === "sign-up" ? (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="firstName"
+                      label="Nome"
+                      placeholder="Primeiro nome"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="lastName"
+                      label="Sobrenome"
+                      placeholder="Último nome"
+                    />
+                  </div>
+                  <CustomInput
+                    control={form.control}
+                    name="address1"
+                    label="Endereço"
+                    placeholder="Insira o seu endereço"
+                  />
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      label="Estado"
+                      placeholder="Ex: SP"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="postalCode"
+                      label="CEP"
+                      placeholder="55555-555"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="birthDate"
+                      label="Data de nascimento"
+                      placeholder="DD/MM/AAAA"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="document"
+                      label="CPF"
+                      placeholder="Ex: 111.222.333.44"
+                    />
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+              <CustomInput
                 control={form.control}
                 name="email"
-                render={({ field }) => (
-                  <div className="form-item">
-                    <FormLabel className="form-label">E-mail</FormLabel>
-                    <div className="flex w-full flex-col">
-                      <FormControl>
-                        <Input
-                          placeholder="Insira seu e-mail"
-                          className="input-class"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage className="form-message mt-2" />
-                    </div>
-                  </div>
-                )}
+                label="E-mail"
+                placeholder="Insira o seu email"
               />
-              <Button type="submit">Submit</Button>
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Senha"
+                placeholder="Insira o sua senha"
+              />
+              <div className="flex flex-col gap-4 ">
+                <Button className="form-btn" type="submit" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Carregando...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Entrar"
+                  ) : (
+                    "Criar conta"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Ainda não possui uma conta?"
+                : "Já possui uma conta?"}
+            </p>
+            <Link
+              className="form-link"
+              href={type === "sign-in" ? "/cadastro" : "/login"}
+            >
+              {type === "sign-in" ? "Criar." : "Entrar."}
+            </Link>
+          </footer>
         </>
       )}
     </section>
